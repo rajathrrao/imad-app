433f3885ce5af1d85119def25a0c8e07f2fdaa65
rajathrrao <31036155+rajathrrao@users.noreply.github.com>
2017-08-16 18:56:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML="New Value";
 //Move the image
 var img=document.getElementById('madi');
 img.onclick=function(){
-    img.style.marginLeft='1000px';
+    img.style.marginLeft='100px';
 };
